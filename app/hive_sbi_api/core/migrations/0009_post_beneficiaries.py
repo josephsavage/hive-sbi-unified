ce9b7e4ed4f50a272d5d0ee59d998ae68bfb99a0
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2026-02-25 11:55:02 -0800

    Update 0009_post_beneficiaries.py

--- app/hive_sbi_api/core/migrations/0009_post_beneficiaries.py
+++ app/hive_sbi_api/core/migrations/0009_post_beneficiaries.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='beneficiaries',
-            field=models.JSONField(null=True),
+            field=models.JSONField(null=False),
         ),
     ]
