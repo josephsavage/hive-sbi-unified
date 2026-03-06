32b0112daaa14ccc19352cfd723fedaa0d15783b
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2026-03-05 21:14:54 -0800

    Update prod.py

--- app/hive_sbi_webapp/settings/prod.py
+++ app/hive_sbi_webapp/settings/prod.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['*']
 # --- API Endpoint Configuration ---
 # Route traffic through Docker's internal network to the 'api' container on port 8009
 SBIAPIURL = os.environ.get('SBI_API_URL', 'http://api:8009')
-SBIAPIURL_V1 = os.environ.get('SBI_API_URL_V1', 'http://api:8009/v1')
+SBIAPIURL_V1 = os.environ.get('SBI_API_URL_V1', 'http://api:8009')
 
 LOGGING = {
     'version': 1,
