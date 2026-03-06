47c2bc26e2b950542dae8b9ea074b51c2369545f
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2026-03-05 20:21:08 -0800

    Update views.py

--- app/hive_sbi_webapp/webapp/views.py
+++ app/hive_sbi_webapp/webapp/views.py
@@ -1,45 +1,43 @@
-# app/hivesbiwebapp/webapp/views.py
-
 import json
 import logging
 import requests
 
 from django.conf import settings
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
-from django.shortcuts import render
 
 from .viewmixins import BaseMixinView
-from .forms import UseInfoForm
+from .forms import UserInfoForm  # Corrected UseInfoForm to UserInfoForm
 
 
 logger = logging.getLogger('webapp')
 
 
 class HomeView(BaseMixinView, TemplateView):
-    template_name = "webapp/home.html"
+    template_name = "webapp/home.html"  # Corrected templatename
 
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
+    # Corrected getcontextdata to standard Django get_context_data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['active_home'] = True
         return context
 
 
-class UserInfoForm(BaseMixinView, TemplateView):
-    templatename = "webapp/userinfoform.html"
+class UserInfoView(BaseMixinView, TemplateView):  # Renamed from UserInfoForm to avoid collision with the actual form
+    template_name = "webapp/userinfoform.html"  # Corrected templatename
 
-    def get_user(self, kwargs):
+    def get_user(self, **kwargs):
         return self.request.GET.get('user')
 
-    def getuserinfoform(self, kwargs):
+    # Aligned method name with how it is called in get_context_data
+    def get_userinfo_form(self, **kwargs):
         user = self.get_user()
-
         initial = {}
 
         if user:
             initial = {'user': user}
 
-        return UseInfoForm(initial=initial)
+        return UserInfoForm(initial=initial)
 
     def get_userinfo(self):
         user = self.request.GET.get("user", "").strip().lower()
@@ -90,7 +88,8 @@ class UserInfoForm(BaseMixinView, TemplateView):
             "status": None,
         }
 
-    def getuserinfohive(self, kwargs):
+    # Aligned method name with how it is called in get_context_data
+    def get_userinfo_hive(self, **kwargs):
         user = self.get_user()
         userinfo_hive = None
 
@@ -113,7 +112,8 @@ class UserInfoForm(BaseMixinView, TemplateView):
             userinfo_hive["error"] = "Connection Error"
             return userinfo_hive
 
-        userinfohive["statuscode"] = response.status_code
+        # Fixed variable typo from userinfohive to userinfo_hive
+        userinfo_hive["status_code"] = response.status_code
 
         if response.status_code == 200:
             try:
@@ -122,14 +122,12 @@ class UserInfoForm(BaseMixinView, TemplateView):
                 userinfo_hive["error"] = "Invalid JSON from Hive API"
                 return userinfo_hive
 
-            # If the v1 endpoint returns the unified wrapper
             if isinstance(content, dict) and "success" in content:
                 userinfo_hive["success"] = content.get("success", False)
                 if userinfo_hive["success"]:
                     userinfo_hive["data"] = content.get("data")
                 else:
                     userinfo_hive["error"] = content.get("error")
-            # If v1 returns raw member data (fallback)
             elif isinstance(content, dict) and "shares" in content:
                 userinfo_hive["success"] = True
                 userinfo_hive["data"] = content
@@ -138,68 +136,61 @@ class UserInfoForm(BaseMixinView, TemplateView):
 
         return userinfo_hive
 
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['active_userinfo'] = True
         context['user'] = self.get_user()
-        context['userinfoform'] = self.getuserinfo_form()
+        context['userinfoform'] = self.get_userinfo_form()
 
         context['userinfo'] = self.get_userinfo()
-        context['userinfohive'] = self.getuserinfo_hive()
+        context['userinfohive'] = self.get_userinfo_hive()
 
         return context
 
 
-# Removed RichListView per project decision to replace with Metabase dashboards.
-
-# If you later want a server-side rich list, reintroduce a dedicated view here.
-
-# Remaining views below unchanged (kept for completeness).
-
-
 class TransactionHistory(BaseMixinView, TemplateView):
-    templatename = "webapp/transactionhistory.html"
+    template_name = "webapp/transactionhistory.html"
 
-    def get_user(self, kwargs):
+    def get_user(self, **kwargs):
         return self.request.GET.get('user')
 
-    def getuserinfoform(self, kwargs):
+    def get_userinfo_form(self, **kwargs):
         user = self.get_user()
-
         initial = {}
 
         if user:
             initial = {'user': user}
 
-        return UseInfoForm(initial=initial)
+        return UserInfoForm(initial=initial)
 
-    def get(self, request, args, *kwargs):
+    # Fixed args/kwargs signature for standard Django get()
+    def get(self, request, *args, **kwargs):
         if self.get_user():
             response = redirect('enrolledhivesbi')
             response['Location'] += '?user={}'.format(self.get_user())
-
             return response
 
-        return super().get(request, args, *kwargs)
+        return super().get(request, *args, **kwargs)
 
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['activetransactionhistory'] = True
         context['activeenrolledhive_sbi'] = False
         context['activesponsoredhive_sbi'] = False
 
-        context['userinfoform'] = self.getuserinfo_form()
+        context['userinfoform'] = self.get_userinfo_form()
         context['user'] = self.get_user()
 
         return context
 
 
 class EnrolledHiveSBI(TransactionHistory):
-    def get(self, request, args, *kwargs):
-        context = self.getcontextdata(kwargs)
-        return self.rendertoresponse(context)
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)  # Fixed rendertoresponse typo
 
-    def getenrolledhive_sbi(self, kwargs):
+    # Aligned method name
+    def get_enrolled_hive_sbi(self, **kwargs):
         LIMIT = 200
 
         try:
@@ -207,7 +198,7 @@ class EnrolledHiveSBI(TransactionHistory):
         except ValueError:
             offset = 0
 
-        enrolledhivesbi = {
+        enrolledhive_sbi = {
             "status_code": None,
             "previous": None,
             "next": None,
@@ -230,220 +221,50 @@ class EnrolledHiveSBI(TransactionHistory):
                 timeout=10,
             )
 
-            enrolledhivesbi["statuscode"] = response.statuscode
+            enrolledhive_sbi["status_code"] = response.status_code
 
             if response.status_code == 200:
                 content = json.loads(response.content.decode("utf-8"))
 
                 if content.get("previous"):
-                    enrolledhivesbi["previous"] = content["previous"].split(
+                    enrolledhive_sbi["previous"] = content["previous"].split(
                         "?")[1].replace('account=', 'user=')
 
                 if content.get("next"):
-                    enrolledhivesbi["next"] = content["next"].split(
+                    enrolledhive_sbi["next"] = content["next"].split(
                         "?")[1].replace('account=', 'user=')
 
                 activepagenumber = offset / LIMIT + 1
 
-                enrolledhivesbi["activepagenumber"] = int(activepagenumber)
-                enrolledhivesbi["prevpagenumber"] = int(activepagenumber - 1)
+                enrolledhive_sbi["activepagenumber"] = int(activepagenumber)
+                enrolledhive_sbi["prevpagenumber"] = int(activepagenumber - 1)
 
                 if offset + 200 < content.get("count", 0):
-                    enrolledhivesbi["nextpagenumber"] = int(activepagenumber + 1)
+                    enrolledhive_sbi["nextpagenumber"] = int(activepagenumber + 1)
 
-                enrolledhivesbi["results"] = content.get("results", [])
+                enrolledhive_sbi["results"] = content.get("results", [])
 
         except requests.exceptions.ConnectionError:
-            enrolledhivesbi["content"] = "Connection Error"
+            enrolledhive_sbi["content"] = "Connection Error"
 
-        return enrolledhivesbi
+        return enrolledhive_sbi
 
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context['activeenrolledhive_sbi'] = True
-
-        context['trxlist'] = self.getenrolledhivesbi()
-
+        context['trxlist'] = self.get_enrolled_hive_sbi()
         return context
 
 
 class SponsoredHiveSBI(TransactionHistory):
-    def get(self, request, args, *kwargs):
-        context = self.getcontextdata(kwargs)
-        return self.rendertoresponse(context)
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
 
-    def getsponsoredhive_sbi(self, kwargs):
+    # Aligned method name
+    def get_sponsored_hive_sbi(self, **kwargs):
         LIMIT = 200
 
         try:
-            offset = int(self.request.GET.get("offset", 0))
-        except ValueError:
-            offset = 0
-
-        sponsoredhivesbi = {
-            "status_code": None,
-            "previous": None,
-            "next": None,
-            "activepagenumber": None,
-            "prevpagenumber": None,
-            "nextpagenumber": None,
-        }
-
-        try:
-            params = "?sponsee={}".format(self.get_user())
-
-            if offset:
-                if params:
-                    params = "{}&offset={}".format(params, offset)
-                else:
-                    params = "?offset={}".format(offset)
-
-            response = requests.get(
-                "{}/v1/transactions/{}".format(settings.SBIAPIURL_V1, params),
-                timeout=10,
-            )
-
-            sponsoredhivesbi["statuscode"] = response.statuscode
-
-            if response.status_code == 200:
-                content = json.loads(response.content.decode("utf-8"))
-
-                if content.get("previous"):
-                    sponsoredhivesbi["previous"] = content["previous"].split(
-                        "?")[1].replace('sponsee=', 'user=')
-
-                if content.get("next"):
-                    sponsoredhivesbi["next"] = content["next"].split(
-                        "?")[1].replace('sponsee=', 'user=')
-
-                activepagenumber = offset / LIMIT + 1
-
-                sponsoredhivesbi["activepagenumber"] = int(activepagenumber)
-                sponsoredhivesbi["prevpagenumber"] = int(activepagenumber - 1)
-
-                if offset + 200 < content.get("count", 0):
-                    sponsoredhivesbi["nextpagenumber"] = int(activepagenumber + 1)
-
-                sponsoredhivesbi["results"] = content.get("results", [])
-
-        except requests.exceptions.ConnectionError:
-            sponsoredhivesbi["content"] = "Connection Error"
-
-        return sponsoredhivesbi
-
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
-        context['activesponsoredhive_sbi'] = True
-
-        context['trxlist'] = self.getsponsoredhivesbi()
-
-        return context
-
-
-class DeliveredVotesView(BaseMixinView, TemplateView):
-    templatename = "webapp/deliveredvotes.html"
-
-    def get_user(self, kwargs):
-        if self.request.GET.get('user'):
-            return self.request.GET.get('user')
-
-        return self.request.GET.get('author')
-
-    def getuserinfoform(self, kwargs):
-        user = self.get_user()
-
-        initial = {}
-
-        if user:
-            initial = {'user': user}
-
-        return UseInfoForm(initial=initial)
-
-    def get_posts(self, kwargs):
-        LIMIT = 200
-
-        ordering = self.request.GET.get("ordering", "")
-
-        try:
-            offset = int(self.request.GET.get("offset", 0))
-        except ValueError:
-            offset = 0
-
-        posts = {
-            "status_code": None,
-            "previous": None,
-            "next": None,
-            "activepagenumber": None,
-            "prevpagenumber": None,
-            "nextpagenumber": None,
-        }
-
-        try:
-            if ordering:
-                params = "?ordering={}".format(ordering)
-            else:
-                params = "?ordering=-created"
-
-            if self.get_user():
-                params = "{}&author={}".format(params, self.get_user())
-
-            if offset:
-                if params:
-                    params = "{}&offset={}".format(params, offset)
-                else:
-                    params = "?offset={}".format(offset)
-
-            response = requests.get(
-                "{}/v1/posts/{}".format(settings.SBIAPIURL_V1, params),
-                timeout=10,
-            )
-
-            posts["statuscode"] = response.statuscode
-
-            if response.status_code == 200:
-                content = json.loads(response.content.decode("utf-8"))
-
-                if content.get("previous"):
-                    posts["previous"] = content["previous"].split(
-                        "?")[1].replace('account=', 'author=')
-
-                if content.get("next"):
-                    posts["next"] = content["next"].split(
-                        "?")[1].replace('account=', 'author=')
-
-                activepagenumber = offset / LIMIT + 1
-
-                posts["activepagenumber"] = int(activepagenumber)
-                posts["prevpagenumber"] = int(activepagenumber - 1)
-
-                if offset + 200 < content.get("count", 0):
-                    posts["nextpagenumber"] = int(activepagenumber + 1)
-
-                posts["results"] = content.get("results", [])
-
-        except requests.exceptions.ConnectionError:
-            posts["content"] = "Connection Error"
-
-        return posts
-
-    def getcontextdata(self, kwargs):
-        context = super().getcontextdata(kwargs)
-        context['activetransactionhistory'] = False
-        context['activeenrolledhive_sbi'] = False
-        context['activesponsoredhive_sbi'] = False
-
-        context['userinfoform'] = self.getuserinfo_form()
-        context['user'] = self.get_user()
-        context['posts'] = self.get_posts()
-
-        ordering = self.request.GET.get("ordering", "")
-
-        if not ordering:
-            context['createddescendingactive'] = True
-
-        if ordering == "created":
-            context['createdascendingactive'] = True
-        if ordering == "-created":
-            context['createddescendingactive'] = True
-
-        return context
+            offset = int(
+                
